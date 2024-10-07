73973a9a3b76e9d89bc2dcdf198122fcb8f54f4b
Selda Larsson <selda.larssonsvensdotter@elev.ga.ntig.se>
2024-10-07 09:28:50 +0200

    1

---
+++ Character.java
@@ -0,0 +1,12 @@
+public class Character {
+
+
+    int hunger = 0;
+    private int boredom = 0;
+    private ArreyList<Sting> words = new ArreyList<String>();
+    private boolean isAlive;
+    public Sting name;
+    Randon ran = new Random();
+
+
+}
